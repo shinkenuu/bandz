dfaaf872a4ffa465fd8a4a0fa15a9d67b4dcd371
shinkenuu <natanael.delatorrejr@live.com>
2017-07-10 18:41:19 -0300

    Add beta models

--- api_/models.py
+++ api_/models.py
@@ -2,14 +2,14 @@ from datetime import datetime
 from django.db import models
 
 USER_TYPES = (
-    (1, u'fa'),
-    (2, u'band'),
-    (3, u'recorder'),
-    (4, u'venue'))
+    (u'f', u'fa'),
+    (u'b', u'band'),
+    (u'r', u'recorder'),
+    (u'h', u'host'))
 
 
 class Location(models.Model):
-    place_id = models.CharField(unique=True, max_length=255, default='', db_index=True)
+    place_id = models.CharField(unique=True, max_length=255, db_index=True)
     street = models.CharField(max_length=100, null=True)
     number = models.IntegerField()
     city = models.CharField(max_length=50, null=True)
@@ -18,17 +18,17 @@ class Location(models.Model):
 
 
 class User(models.Model):
-    first_name = models.CharField(null=False, default='', max_length=30)
-    last_name = models.CharField(null=False, default='', max_length=50)
-    email = models.EmailField(null=False, default='', unique=True, db_index=True)
-    type = models.IntegerField(choices=USER_TYPES)
+    first_name = models.CharField(null=False, max_length=30)
+    last_name = models.CharField(null=False, max_length=50)
+    email = models.EmailField(null=False, unique=True, db_index=True)
+    type = models.CharField(max_length=10, choices=USER_TYPES)
     created = models.DateTimeField(auto_now_add=True)
-    location = models.ForeignKey(Location, default=-1)
+    location = models.ForeignKey(Location)
 
 
 class Band(models.Model):
     user_id = models.ForeignKey(User, null=True)
-    name = models.CharField(null=False, default='', max_length=80)
+    name = models.CharField(null=False, max_length=80)
     genre = models.CharField(max_length=20)
     url = models.URLField()
     fans_amount = models.BigIntegerField()
@@ -36,17 +36,17 @@ class Band(models.Model):
 
 class Host(models.Model):
     user_id = models.ForeignKey(User, null=True)
-    name = models.CharField(null=False, default='', max_length=80)
+    name = models.CharField(null=False, max_length=80)
     url = models.URLField()
     max_capacity = models.BigIntegerField()
-    location = models.ForeignKey(Location, default=-1)
+    location = models.ForeignKey(Location)
 
 
 class Event(models.Model):
-    name = models.CharField(null=False, default='', max_length=80)
-    location = models.ForeignKey(Location, default=-1)
-    starts_at = models.DateTimeField(null=False, default=datetime.min)
-    ends_at = models.DateTimeField(null=False, default=datetime.max)
+    name = models.CharField(null=False, max_length=80)
+    location = models.ForeignKey(Location)
+    starts_at = models.DateTimeField(null=False)
+    ends_at = models.DateTimeField(null=False)
     url = models.URLField()
     # presenting
-    host = models.ForeignKey(Host, default=-1)
+    host = models.ForeignKey(Host)
