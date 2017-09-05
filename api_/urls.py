1f3d9832511132f0e323f527c431df0796081bb9
shinkenuu <natanael.delatorrejr@live.com>
2017-09-05 02:15:01 -0300

    Updat urls for new models

--- api_/urls.py
+++ api_/urls.py
@@ -13,26 +13,50 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from rest_framework.authtoken import views as rest_auth_view
-from api_ import views
+from api_.views import PlaceDetail, PlaceList, MusicGenreDetail, MusicGenreList, UserDetail, UserList, index,\
+    FaDetail, FaList, BandDetail, BandList, HostDetail, HostList, EventDetail, EventList,\
+    PropositionDetail, PropositionList, EventPublicDetail, EventPublicList
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', index),
     url(r'^auth-token/$', rest_auth_view.obtain_auth_token),
-    url(r'^locations/$', views.LocationList.as_view()),
-    url(r'^locations/(?P<pk>[0-9]+)/$', views.LocationDetail.as_view()),
-    url(r'^locations/(?P<placeid>[a-zA-Z0-9]+)/$', views.LocationDetail.as_view()),
-    url(r'^users/$', views.UserList.as_view()),
-    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
-    url(r'^bands/$', views.BandList.as_view()),
-    url(r'^bands/(?P<pk>[0-9]+)/$', views.BandDetail.as_view()),
-    url(r'^bands/(?P<name>[a-zA-Z0-9]+)/$', views.BandDetail.as_view()),
-    url(r'^hosts/$', views.HostList.as_view()),
-    url(r'^hosts/(?P<pk>[0-9]+)/$', views.HostDetail.as_view()),
-    url(r'^hosts/(?P<location_id>[0-9]+)/$', views.HostDetail.as_view()),
-    url(r'^hosts/(?P<name>[a-zA-Z0-9]+)/$', views.HostDetail.as_view()),
-    url(r'^events/$', views.EventList.as_view()),
-    url(r'^events/(?P<pk>[0-9]+)/$', views.EventDetail.as_view()),
-    url(r'^events/(?P<location_id>[0-9]+)/$', views.EventDetail.as_view()),
+    url(r'^music-genres/', include([
+        url(r'^$', MusicGenreList.as_view()),
+        url(r'^(?P<pk>\d+)/$', MusicGenreDetail.as_view()),
+    ], 'music_genres')),
+    url(r'^places/', include([
+        url(r'^$', PlaceList.as_view()),
+        url(r'^(?P<pk>[a-zA-Z0-9]+)/$', PlaceDetail.as_view()),
+    ], 'places')),
+    url(r'^users/', include([
+        url(r'^$', UserList.as_view()),
+        url(r'(?P<pk>\d+)/$', UserDetail.as_view()),
+        url(r'(?P<username>[a-zA-Z0-9\-]+)/$', UserDetail.as_view()),
+    ], 'users')),
+    url(r'^fas/', include([
+        url(r'^$', FaList.as_view()),
+        url(r'^(?P<pk>\d+)/$', FaDetail.as_view()),
+    ], 'fas')),
+    url(r'^bands/', include([
+        url(r'^$', BandList.as_view()),
+        url(r'^(?P<pk>\d+)/$', BandDetail.as_view()),
+    ], 'bands')),
+    url(r'^hosts/', include([
+        url(r'^$', HostList.as_view()),
+        url(r'^(?P<pk>\d+)/$', HostDetail.as_view()),
+    ], 'hosts')),
+    url(r'^events/', include([
+        url(r'^$', EventList.as_view()),
+        url(r'^(?P<pk>\d+)/$', EventDetail.as_view()),
+    ], 'events')),
+    url(r'^propositions/', include([
+        url(r'^$', PropositionList.as_view()),
+        url(r'^(?P<pk>\d+)/$', PropositionDetail.as_view()),
+    ], 'propositions')),
+    url(r'^event-public/', include([
+        url(r'^$', EventPublicList.as_view()),
+        url(r'^(?P<pk>\d+)/$', EventPublicDetail.as_view()),
+    ], 'event-public')),
 ]
