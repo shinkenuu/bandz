413938751a4df7d3db6c221b16b943b9626d64b4
shinkenuu <natanael.delatorrejr@live.com>
2017-09-05 02:16:01 -0300

    Updated APIViews for updated models. Edited Propositions update behaviour

--- api_/views.py
+++ api_/views.py
@@ -1,7 +1,11 @@
 from django.http import HttpResponse
-from rest_framework import authentication, generics, permissions
-from api_ import models
-from bandz import serializers
+from django.shortcuts import render
+from rest_framework.status import HTTP_200_OK
+from rest_framework.generics import ListAPIView, RetrieveAPIView, \
+    ListCreateAPIView, RetrieveUpdateDestroyAPIView, get_object_or_404
+from .models import Place, MusicGenre, User, Fa, Band, Host, Event, Proposition, EventPublic
+from bandz.serializers import PlaceSerializer, MusicGenreSerializer, UserSerializer, FaSerializer, BandSerializer, \
+    HostSerializer, EventSerializer, NewPropositionSerializer, ExistingPropositionSerializer, EventPublicSerializer
 
 
 def index(request):
@@ -10,73 +14,108 @@ def index(request):
     :param request: 
     :return: 
     """
-    return HttpResponse(content='Yeah, Im alive')
+    return render(request, template_name='api/index.html', context={})
 
 
-class LocationList(generics.ListCreateAPIView):
-    queryset = models.Location.objects.all()
-    serializer_class = serializers.LocationSerializer
-    # authentication_classes = (authentication.TokenAuthentication, )
-    # permission_classes = (permissions.IsAuthenticated, )
+class PlaceList(ListCreateAPIView):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
 
 
-class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = models.Location.objects.all()
-    serializer_class = serializers.LocationSerializer
-    # authentication_classes = (authentication.TokenAuthentication, )
-    # permission_classes = (permissions.IsAuthenticated, )
+class PlaceDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Place.objects.all()
+    serializer_class = PlaceSerializer
 
 
-class UserList(generics.ListCreateAPIView):
-    queryset = models.User.objects.all()
-    serializer_class = serializers.UserSerializer
-    # authentication_classes = (authentication.TokenAuthentication, )
-    # permission_classes = (permissions.IsAuthenticated, )
+class MusicGenreList(ListAPIView):
+    queryset = MusicGenre.objects.all()
+    serializer_class = MusicGenreSerializer
 
 
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = models.User.objects.all()
-    serializer_class = serializers.UserSerializer
-    # authentication_classes = (authentication.TokenAuthentication, )
-    # permission_classes = (permissions.IsAuthenticated, )
+class MusicGenreDetail(RetrieveAPIView):
+    queryset = MusicGenre.objects.all()
+    serializer_class = MusicGenreSerializer
 
 
-class BandList(generics.ListCreateAPIView):
-    queryset = models.Band.objects.all()
-    serializer_class = serializers.BandSerializer
-    authentication_classes = (authentication.TokenAuthentication, )
-    permission_classes = (permissions.IsAuthenticated, )
+class UserList(ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-class BandDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = models.Band.objects.all()
-    serializer_class = serializers.BandSerializer
-    # authentication_classes = (authentication.TokenAuthentication, )
-    # permission_classes = (permissions.IsAuthenticated, )
+class UserDetail(RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 
-class HostList(generics.ListCreateAPIView):
-    queryset = models.Host.objects.all()
-    serializer_class = serializers.HostSerializer
-    # authentication_classes = (authentication.TokenAuthentication, )
-    # permission_classes = (permissions.IsAuthenticated, )
-
-class HostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = models.Host.objects.all()
-    serializer_class = serializers.HostSerializer
-    # authentication_classes = (authentication.TokenAuthentication, )
-    # permission_classes = (permissions.IsAuthenticated, )
+class FaList(ListCreateAPIView):
+    queryset = Fa.objects.all()
+    serializer_class = FaSerializer
 
 
-class EventList(generics.ListCreateAPIView):
-    queryset = models.Event.objects.all()
-    serializer_class = serializers.EventSerializer
-    # authentication_classes = (authentication.TokenAuthentication, )
-    # permission_classes = (permissions.IsAuthenticated, )
+class FaDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Fa.objects.all()
+    serializer_class = FaSerializer
 
 
-class EventDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = models.Event.objects.all()
-    serializer_class = serializers.EventSerializer
-    # authentication_classes = (authentication.TokenAuthentication, )
-    # permission_classes = (permissions.IsAuthenticated, )
+class BandList(ListCreateAPIView):
+    queryset = Band.objects.all()
+    serializer_class = BandSerializer
+
+
+class BandDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Band.objects.all()
+    serializer_class = BandSerializer
+
+
+class HostList(ListCreateAPIView):
+    queryset = Host.objects.all()
+    serializer_class = HostSerializer
+
+
+class HostDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Host.objects.all()
+    serializer_class = HostSerializer
+
+
+class EventList(ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class EventDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class PropositionList(ListCreateAPIView):
+    queryset = Proposition.objects.all()
+    serializer_class = NewPropositionSerializer
+
+
+class PropositionDetail(RetrieveUpdateDestroyAPIView):
+    queryset = Proposition.objects.all()
+    serializer_class = ExistingPropositionSerializer
+
+    def update(self, request, *args, **kwargs):
+        response = super().update(request, *args, **kwargs)
+
+        if response.status_code == HTTP_200_OK and response.data.get('confirmed', False):
+            instance = self.get_object()
+            event_filter = {
+                'pk': instance.event_id
+            }
+            event = get_object_or_404(Event.objects.all(), **event_filter)
+            event.band = instance.band
+            event.confirmed = True
+            event.save()
+        return response
+
+
+class EventPublicList(ListCreateAPIView):
+    queryset = EventPublic.objects.all()
+    serializer_class = EventPublicSerializer
+
+
+class EventPublicDetail(RetrieveUpdateDestroyAPIView):
+    queryset = EventPublic.objects.all()
+    serializer_class = EventPublicSerializer
