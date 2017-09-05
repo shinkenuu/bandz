51bf91c8fa4d8e0e1b7e323a46607725021d515b
shinkenuu <natanael.delatorrejr@live.com>
2017-09-05 02:13:46 -0300

    Updated models for MVP only

--- api_/models.py
+++ api_/models.py
@@ -1,11 +1,7 @@
 from django.contrib.auth.models import User
-from django.db import models
-
-USER_TYPES_CHOICES = (
-    (u'f', u'fa'),
-    (u'b', u'band'),
-    (u'h', u'host'),
-)
+from django.db.models import Model, CharField, TextField, DecimalField, DateTimeField, DateField, BigIntegerField,\
+    PositiveSmallIntegerField, BooleanField, NullBooleanField, URLField, ForeignKey, OneToOneField, ManyToManyField
+from django.utils import timezone
 
 EVENT_RATING_CHOICES = (
     (1, 'bad'),
@@ -16,159 +12,123 @@ EVENT_RATING_CHOICES = (
 )
 
 FAN_AT_EVENT_FEELING_CHOICES = (
-    (u'm', 'maybe'),
     (u'i', 'interested'),
     (u'n', 'nah'),
     (u'g', 'going'),
 )
 
 
-class Location(models.Model):
-    place_id = models.CharField(unique=True, max_length=255, db_index=True)
-    street = models.CharField(max_length=100, null=True)
-    number = models.IntegerField()
-    city = models.CharField(max_length=50, null=True)
-    state = models.CharField(max_length=2, null=True)
-    country = models.CharField(max_length=30, null=True)
+class Place(Model):
+    id = CharField(unique=True, max_length=255, primary_key=True)
+    formatted_address = CharField(max_length=255, null=True)
+    latitude = DecimalField(decimal_places=6, max_digits=9, null=True)
+    longitude = DecimalField(decimal_places=6, max_digits=9, null=True)
 
     def __str__(self):
-        return '{}, n{} - {} - {} - {}'.format(
-            self.street, self.number, self.city, self.state, self.country)
+        return self.formatted_address
 
 
-class MusicGenre(models.Model):
-    name = models.CharField(unique=True, max_length=30)
-    parent_genre = models.ForeignKey('MusicGenre', null=True)
+class MusicGenre(Model):
+    name = CharField(unique=True, max_length=30)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name_plural = 'Music Genres'
 
     def __str__(self):
-        return '{} music genre'.format(self.name)
+        return self.name
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    type = models.CharField(choices=USER_TYPES_CHOICES, max_length=1, default='f')
-    created = models.DateTimeField(auto_now_add=True)
+class UserProfile(Model):
+    user = OneToOneField(User)
+    firebase_uid = CharField(max_length=255, null=True)
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return self.user.EMAIL_FIELD
 
 
-class Fa(models.Model):
-    user_profile = models.ForeignKey(UserProfile)
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=50)
-    birth_date = models.DateField()
-    location = models.ForeignKey(Location)
-    favorite_genres = models.ManyToManyField('MusicGenre', through='FaMusicTaste', through_fields=('fa', 'music_genre'))
-    events = models.ManyToManyField('Event', through='FaAtEvent', through_fields=('fa', 'event'))
+class Fa(UserProfile):
+    birth_date = DateField(default=timezone.now)
+    place = ForeignKey(Place)
+    music_genres = ManyToManyField('MusicGenre')
+    events = ManyToManyField('Event', through='EventPublic', through_fields=('fa', 'event'))
 
     def __str__(self):
-        return 'Fa {} {}'.format(self.first_name, self.last_name)
+        return self.user.get_full_name()
 
 
-class Band(models.Model):
-    user_profile = models.ForeignKey(UserProfile, null=True)
-    name = models.CharField(max_length=80)
-    description = models.TextField()
-    genre = models.ManyToManyField('MusicGenre', through='BandGenre', through_fields=('band', 'music_genre'))
-    url = models.URLField()
-    fans_amount = models.BigIntegerField(default=0)
-    location = models.ForeignKey(Location)
-    events = models.ManyToManyField('Event', through='BandAtEvent', through_fields=('band', 'event'))
+class Band(UserProfile):
+    name = CharField(max_length=80)
+    description = TextField()
+    url = URLField()
+    place = ForeignKey(Place)
+    music_genres = ManyToManyField('MusicGenre')
 
     def __str__(self):
-        return 'Band {}'.format(self.name)
+        return self.name
 
 
-class Host(models.Model):
-    user_profile = models.ForeignKey(User, null=True)
-    name = models.CharField(max_length=80)
-    description = models.TextField(null=True)
-    url = models.URLField()
-    max_capacity = models.BigIntegerField()
-    location = models.ForeignKey(Location)
-    events = models.ManyToManyField('Event', through='EventAtHost', through_fields=('host', 'event'))
-
-    def __str__(self):
-        return 'Host {}'.format(self.name)
-
-
-class Event(models.Model):
-    name = models.CharField(max_length=80)
-    starts_at = models.DateTimeField()
-    ends_at = models.DateTimeField()
-    min_age = models.PositiveSmallIntegerField()
-    url = models.URLField()
-    presenting_bands = models.ManyToManyField('Band', through='BandAtEvent', through_fields=('event', 'band'))
-    hosts = models.ManyToManyField('Host', through='EventAtHost', through_fields=('event', 'host'))
+class Host(UserProfile):
+    name = CharField(max_length=80)
+    description = TextField()
+    url = URLField()
+    max_capacity = BigIntegerField()
+    place = ForeignKey(Place)
+    music_genres = ManyToManyField('MusicGenre')
 
     def __str__(self):
-        return 'Event '.format(self.name)
+        return self.name
 
 
-class Proposition(models.Model):
-    band = models.ForeignKey(Band)
-    host = models.ForeignKey(Host)
-    message = models.TextField(null=False)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    starts_at = models.DateTimeField()
-    ends_at = models.DateTimeField()
-    closed = models.BooleanField(default=False)
+class Event(Model):
+    name = CharField(max_length=80)
+    description = TextField()
+    url = URLField(null=True)
+    starts_at = DateTimeField()
+    ends_at = DateTimeField()
+    min_age = PositiveSmallIntegerField()
+    music_genres = ManyToManyField('MusicGenre')
+    price = DecimalField(max_digits=6, decimal_places=2)
+    host = ForeignKey(Host, related_name='events')
+    band = ForeignKey(Band, related_name='events', null=True)
+    confirmed = BooleanField(default=False)
+    public = ManyToManyField('Fa', through='EventPublic', through_fields=('event', 'fa'))
 
-    def __str__(self):
-        return 'Prop between band {} and host {}'.format(self.band.name, self.host.name)
-
-
-class EventAtHost(models.Model):
-    event = models.ForeignKey(Event)
-    host = models.ForeignKey(Host)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-
-    def __str__(self):
-        return 'Event {} at host {}'.format(self.event.name, self.host.name)
-
-
-class BandAtEvent(models.Model):
-    band = models.ForeignKey(Band)
-    event = models.ForeignKey(Event)
-    starts_at = models.DateTimeField(null=False)
-    ends_at = models.DateTimeField(null=False)
+    class Meta:
+        unique_together = (('name', 'host', 'starts_at'), )
 
     def __str__(self):
-        return 'Band {} at event {}'.format(self.band.name, self.event.name)
+        return self.name
 
 
-class FaAtEvent(models.Model):
-    fa = models.ForeignKey(Fa)
-    event = models.ForeignKey(Event)
-    feeling = models.CharField(null=True, choices=FAN_AT_EVENT_FEELING_CHOICES, max_length=1)
-    rating = models.PositiveSmallIntegerField(null=True, choices=EVENT_RATING_CHOICES)
+class Proposition(Model):
+    band = ForeignKey(Band, related_name='propositions')
+    host = ForeignKey(Host, related_name='propositions')
+    event = ForeignKey(Event, related_name='propositions')
+    message = TextField(null=False)
+    price = DecimalField(max_digits=6, decimal_places=2)
+    confirmed = NullBooleanField(default=None)
 
-    def __str__(self):
-        return 'Fa {} about event {}'.format(self.fa.first_name, self.event.name)
-
-
-class Fa2Band(models.Model):
-    band = models.ForeignKey(Band)
-    fa = models.ForeignKey(Fa)
-    subscribed = models.BooleanField(default=False)
+    class Meta:
+        unique_together = (('band', 'event'), )
 
     def __str__(self):
-        return 'Fa {} about Band {}'.format(self.fa.first_name, self.band.name)
+        return '{}\'s to play at {}'.format(self.band.name, self.event.name)
 
 
-class FaMusicTaste(models.Model):
-    fa = models.ForeignKey(Fa)
-    music_genre = models.ForeignKey(MusicGenre)
+class EventPublic(Model):
+    fa = ForeignKey(Fa)
+    event = ForeignKey(Event)
+    feeling = CharField(null=True, choices=FAN_AT_EVENT_FEELING_CHOICES, max_length=1)
+    rating = PositiveSmallIntegerField(null=True, choices=EVENT_RATING_CHOICES)
+    comment = TextField(null=True)
 
-    def __str__(self):
-        return 'Fa {} likes {} music'.format(self.fa.first_name, self.music_genre.name)
-
-
-class BandGenre(models.Model):
-    band = models.ForeignKey(Band)
-    music_genre = models.ForeignKey(MusicGenre)
-    degree = models.PositiveSmallIntegerField()
+    class Meta:
+        verbose_name_plural = "Event's Public"
+        unique_together = (('fa', 'event'), )
 
     def __str__(self):
-        return 'Band {} plays {} music'.format(self.band.name, self.music_genre.name)
+        return '{} about {}'.format(self.fa.user.get_full_name(), self.event.name)
